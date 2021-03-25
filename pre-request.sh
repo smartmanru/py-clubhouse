30a9ff063abf1fdc8707fd2b907641dbbe6e4d35
VSPyzhenkov <vladimir.pyzhenkov@\domrf.ru>
2021-03-25 21:26:14 +0300

    add

--- pre-request.sh
+++ pre-request.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" -y 
 brew install numpy scipy ipython jupyter git 
-git clone 
+git clone https://github.com/smartmanru/py-clubhouse ~/py-club
+cd ~/py-club
 python3 -m pip install -r requirements.txt
+python3 -m pip install agora agora-python-sdk
+python3 v2.py
