521c90cf5a3f3cae2352115716bec9cc77a87f17
VSPyzhenkov <vladimir.pyzhenkov@\domrf.ru>
2021-03-25 21:22:44 +0300

    add mac scripy pre-request

---
+++ pre-request.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" -y 
+brew install numpy scipy ipython jupyter git 
+git clone 
+python3 -m pip install -r requirements.txt
